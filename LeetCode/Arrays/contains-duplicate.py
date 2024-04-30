2f71124f1237a5e9d753d0525a3c42e7c6eab55f
Mahnoor <mahnoor_shahid_shakir@outlook.com>
2024-04-30 05:45:22 +0200

    leet_code files

---
+++ LeetCode/Arrays/contains-duplicate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
+
+obj1 = Solution()
+print(obj1.containsDuplicate(nums=[1, 3, 2]))
+
+
